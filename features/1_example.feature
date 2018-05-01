749390cbb5f5750ad7fae38844127ed8c88d6830
Kyle Moore <kyle.moore@cgifederal.com>
2018-05-01 16:38:43 -0400

    Initial commit

---
+++ features/1_example.feature
@@ -0,0 +1,6 @@
+Feature: Example, meaningless feature file
+
+  Scenario: A test runs in parallel
+    Given something
+    When someone gives me a parallel CL argument
+    Then I run in parallel
---
+++ features/step-definitions/steps.js
@@ -0,0 +1,5 @@
+const {Given, When, Then} = require('cucumber');
+
+Given('something', function(){});
+When('someone gives me a parallel CL argument', function(){});
+Then('I run in parallel', function(){});
