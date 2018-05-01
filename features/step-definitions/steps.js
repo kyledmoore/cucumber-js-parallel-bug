15540ea695229fdea42737aaa3b463f850a0f52c
Kyle Moore <kyle.moore@cgifederal.com>
2018-05-01 16:59:20 -0400

    Add 1 second delay to given step to illustrate perf gains when working
    Note error is not encountered in OS X

--- features/step-definitions/steps.js
+++ features/step-definitions/steps.js
@@ -1,5 +1,7 @@
 const {Given, When, Then} = require('cucumber');
 
-Given('something', function(){});
+Given('something', function(cb){
+    setTimeout(cb, 1000);
+});
 When('someone gives me a parallel CL argument', function(){});
 Then('I run in parallel', function(){});
